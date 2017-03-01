37fd49699a1056b0157f14fc92f3bc54e85689e3
Bmar21 <brandonmalor21@gmail.com>
2017-03-01 12:52:00 -0700

    made the DoublyLinkedList class

---
+++ MegaData/Model/DoublyLinkedList.hpp
@@ -0,0 +1,21 @@
+//
+//  DoublyLinkedList.hpp
+//  MegaData
+//
+//  Created by Marlor, Brandon on 3/1/17.
+//  Copyright © 2017 Marlor, Brandon. All rights reserved.
+//
+
+#ifndef DoublyLinkedList_h
+#define DoublyLinkedList_h
+
+#include "BiDirectionalNode.hpp"
+
+template <class Type>
+class DoublyLinkedList
+{
+    
+};
+
+
+#endif /* DoublyLinkedList_h */
